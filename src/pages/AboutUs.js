31cdabf897039bd3936e64e8cb057f9a4621042a
Angelo Nazzaro <mutenssj@gmail.com>
2022-12-25 09:12:45 +0100

    about us page base structure

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,13 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FAQSection from "../components/FAQSection";
 
 function AboutUs() {
     return (
         <div>
             <AboutSection />
+            <ServicesSection />
+            <FAQSection />
         </div>
     );
 }
