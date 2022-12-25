4262eef363a8ca49d9de2039f036ab0c413d6ad3
Angelo Nazzaro <mutenssj@gmail.com>
2022-12-25 09:01:13 +0100

    about us structure

---
+++ src/components/AboutSection.js
@@ -0,0 +1,30 @@
+import home1 from '../img/home1.png';
+
+function AboutSection() {
+    return (
+        <div>
+            <div className="description">
+                <div className="title">
+                    {/* Separating the text in different chunks that will be
+                        animated independently */}
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>your <span>dreams</span> come </h2>
+                    </div>
+                     <div className="hide">
+                        <h2>true.</h2>
+                    </div>
+                </div>
+                <p>Contact us for any photography or video ideas that you have.</p>
+                <button>Contact Us</button>
+            </div>
+            <div className="image">
+                <img src={home1} alt="guy with a camera" />
+            </div>
+        </div>
+    );
+}
+
+export default AboutSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import AboutSection from "../components/AboutSection";
+
+function AboutUs() {
+    return (
+        <div>
+            <AboutSection />
+        </div>
+    );
+}
+
+export default AboutUs;
