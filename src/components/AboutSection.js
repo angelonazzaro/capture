3d364c03d225386d2673752cebe4a2d87aeb2747
Angelo Nazzaro <mutenssj@gmail.com>
2022-12-25 09:33:53 +0100

    global and about section style

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,30 +1,69 @@
 import home1 from '../img/home1.png';
+// Import Styled
+import styled from 'styled-components'; 
 
 function AboutSection() {
     return (
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
                     {/* Separating the text in different chunks that will be
                         animated independently */}
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>your <span>dreams</span> come </h2>
-                    </div>
-                     <div className="hide">
+                    </Hide>
+                     <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any photography or video ideas that you have.</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="guy with a camera" />
-            </div>
-        </div>
+            </Image>
+        </About>
     );
 }
+
+// Styled Components -> styled.DOMElement you want to style`{style}`
+// To use this, you just substitute the DOM element you want to style with the Styled Components
+const About = styled.div`
+    min-height: 90vh;
+
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 5rem 10rem;
+    color: white;
+`; 
+
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+
+    h2 {
+        font-weight: lighter;
+    }
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+const Image = styled.div`
+    flex: 1;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
 
 export default AboutSection;
